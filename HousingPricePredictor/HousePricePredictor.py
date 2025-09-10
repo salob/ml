be32eb620f7b3e01dc423de729559564409c588a
salob <sianob79@gmail.com>
2025-09-10 18:18:56 +0100

    install and test out tools

--- HousingPricePredictor/HousePricePredictor.py
+++ HousingPricePredictor/HousePricePredictor.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from codecarbon import EmissionsTracker
 
 from sklearn.datasets import fetch_california_housing
 from sklearn.linear_model import LinearRegression
@@ -11,44 +12,26 @@ from sklearn.model_selection import GridSearchCV
 import warnings
 warnings.filterwarnings('ignore')
 
-housing=fetch_california_housing()
-housing
+tracker = EmissionsTracker()
+tracker.start()
 
+housing=fetch_california_housing()
 data=pd.DataFrame(housing['data'],columns=housing['feature_names'])
 df=data.copy()
-
-# Add the target variable to the dataframe
-
 df['MedHouseVal']=housing['target']
-
 df.head()
-
 df.shape
-
 df.size
-
 df.columns
-
 df.info()
-
-
-# Clean
-
 df.isnull().sum()
-
-# Summarise
-
 df.describe(include='all')
-
-# Compute the correlation matrix
-
 corr_matrix=df.corr()
-
-# Create a customized heatmap
-
 fig,ax=plt.subplots(figsize=(10,8))
 sns.heatmap(corr_matrix,annot=True,fmt='.2f',cmap='coolwarm')
 plt.title('Correlation Matrix',fontsize=18)
 plt.xlabel('Variables',fontsize=14)
 plt.ylabel('Variables',fontsize=14)
 plt.show()
+
+tracker.stop()
